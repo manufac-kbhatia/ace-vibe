e50e107baf6146d30f72dce855ac771c4d4802a9
manufac-kbhatia <bhatiakunal3256@gmail.com>
2025-08-02 23:13:25 +0530

    03-trpc-boilerplate-added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+"use client";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
+  const trpc = useTRPC();
+  const {data} = useQuery(trpc.hello.queryOptions({text: "Kunal"}));
+
   return (
-    <div className="text-3xl">Hello World!</div>
+    <div className="text-3xl">Hello {data?.greeting}</div>
   );
 }
