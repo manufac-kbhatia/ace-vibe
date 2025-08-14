a49b03c2fe13bf02b3983eb8d286f302fdbd1617
manufac-kbhatia <bhatiakunal3256@gmail.com>
2025-08-14 19:33:32 +0530

    trpc and inngest removed as no longer needed for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,8 @@
-"use client";
-import { useTRPC } from "@/trpc/client";
-import { useMutation } from "@tanstack/react-query";
-
 export default function Home() {
-  const trpc = useTRPC();
-  const {mutate} = useMutation(trpc.invoke.mutationOptions());
-  
+
   return (
     <div className="text-3xl">
-      <button className="border-2 border-red-900 p-4 text-2xl" onClick={() => {
-        mutate({value: "Kunal bhatia"})
-      }}>invoke</button>
+      <button className="border-2 border-red-900 p-4 text-2xl">invoke</button>
     </div>
   );
 }
--- src/trpc/routers/_app.ts
+++
@@ -1,7 +0,0 @@
-import { createTRPCRouter } from '../init';
-import * as procedures from './procedures';
-export const appRouter = createTRPCRouter({
-  invoke: procedures.invokeProcedure,
-});
-// export type definition of API
-export type AppRouter = typeof appRouter;
