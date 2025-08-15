8ad35de9abfb230edcd3285f27c683858fbb030c
manufac-kbhatia <bhatiakunal3256@gmail.com>
2025-08-15 18:28:10 +0530

    Removed trpc provider frpm layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { TRPCReactProvider } from "@/trpc/client";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +23,6 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <TRPCReactProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -32,6 +30,5 @@ export default function RootLayout({
         {children}
       </body>
     </html>
-    </TRPCReactProvider>
   );
 }
